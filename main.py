4bb9d2c2cf9c62a468211582a1d39bf54857a6bc
YaCpotato <yasshisshy@gmail.com>
2021-05-04 20:07:05 +0900

    操作対象dfをpandasに変更

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import base
 import random
+import os
+import pandas as pd
 
 # command line value
 import argparse
@@ -11,9 +12,10 @@ import termcolor
 import enquiries
 ua_create_categorical = termcolor.colored('Add categorical column: ', 'green')
 ua_create_numeric = termcolor.colored('Add numeric column: ', 'green')
+ua_preview = termcolor.colored('Preview dataframe head: ', 'green')
 ua_publish = 'Complete creation and publish data.'
-ua_exit = 'Stop creating data and EXIT'
-actions = [ua_create_categorical, ua_create_numeric, ua_publish, ua_exit]
+ua_exit = termcolor.colored('Stop creating data and EXIT', 'yellow')
+actions = [ua_create_categorical, ua_create_numeric, ua_preview, ua_publish, ua_exit]
 
 guide_1 = termcolor.colored('Enter length of dataframe: ', 'green')
 guide_2 = termcolor.colored('Enter the column name: ', 'green')
@@ -22,46 +24,68 @@ guide_4 = termcolor.colored('Enter the percentages of the values you entered in 
 
 retry = termcolor.colored('must be a number. please retry.', 'red')
 
+df = pd.DataFrame()
+
+def do_action():
+    global df
+    # choose action
+    user_action = enquiries.choose('Choose Action: ', actions)
+    
+    # if publish or exit
+    if user_action == ua_publish:
+        print(ua_publish)
+        os.exit
+    elif user_action == ua_exit:
+        os.exit
+    elif user_action == ua_preview:
+        print(df.head())
+        do_action()
+    
+    # if create column...
+
+    # define column name
+    col_name = input(guide_2)
+        
+    if user_action == ua_create_categorical:
+        values = input(guide_3).split(' ')
+        values_percentage = [int(n) for n in input(guide_4).split(' ')]
+        series = create_categorical(col_name, values, values_percentage)
+        df.assign(series)
+        print('colum added.')
+    elif user_action == ua_create_numeric:
+        print('this selection not supported yet.')
+
+
+def create_categorical(col_name, values, values_percentage):
+    # if first columns, normal create
+    series_value = random.choices(population = values, weights = values_percentage, k = length)
+    column = pd.Series(series_value, name = col_name, dtype='str')
+    return Series
+
 if __name__ == '__main__':
+    # parser = argparse.ArgumentParser()
+    # #parser.add_argument("pass", help = "the yaml file you use here", type = str)
+    # parser.add_argument("-i","--index", help = "dataframe's index", type = str, default='cs')
+    # args = parser.parse_args()
     
-    df = base.DummyFrame()
+    # if args.index == 'ts':
+    #     print('Create time series data.')
+    # elif args.index == 'cs':
+    #     print('Create cross section data.')
+    # else:
+    #     pass 
     
-    parser = argparse.ArgumentParser()
-    #parser.add_argument("pass", help = "the yaml file you use here", type = str)
-    parser.add_argument("-i","--index", help = "dataframe's index", type = str, default='cs')
-    args = parser.parse_args()
-    
-    if args.index == 'ts':
-        print('Create time series data.')
-    elif args.index == 'cs':
-        print('Create cross section data.')
-    else:
-        pass 
-    
+    # define dataframe length
     try:
         length = int(input(guide_1))
     except:
         print(retry)
         length = int(input(guide_1))
-    
-    col_name = input(guide_2)
-    values = input(guide_3).split(' ')
-    values_percentage = [int(n) for n in input(guide_4).split(' ')]
-    
-    a = random.choices(population = values, weights = values_percentage, k = length)
-    
-    user_action = enquiries.choose('Choose Action: ', actions)
-    
-    if user_action == ua_create_categorical:
-        print(ua_create_categorical)
-    elif user_action == ua_create_numeric:
-        print(ua_create_numeric)
-    elif user_action == ua_publish:
-        print(ua_publish)
-    elif user_action == ua_exit:
-        print(ua_exit)
-    else:
-        print('OOOOOO')
+        
+    try:
+        do_action()
+    finally:
+        do_action()
     
     
     
